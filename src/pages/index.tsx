f3148d981c30699c4265aafbb7bce9b44ebecee4
Max Goodhart <c@chromakode.com>
2020-11-21 21:07:56 -0800

    Cleanup

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react'
-import { useEffect, useLayoutEffect, useState, useRef } from 'react'
+import { useLayoutEffect, useState, useRef } from 'react'
 import {
   Box,
   Button,
