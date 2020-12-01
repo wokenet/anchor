b3c5ba50b8da04205d512c326e417c7416b9865b
Max Goodhart <c@chromakode.com>
2020-12-01 10:22:16 -0800

    Add support for emote aliases

--- src/components/Message.tsx
+++ src/components/Message.tsx
@@ -1,20 +1,36 @@
 import * as React from 'react'
 import { Img, Text } from '@chakra-ui/react'
+import escapeStringRegexp from 'escape-string-regexp'
 
 import { emoteSize } from '../../constants.json'
 
-const emoteRequire = require.context('woke-content/emotes/', false, /\.png$/)
-const emotes = new Map<string, string>(
-  emoteRequire.keys().map((k) => {
-    const emoteText = k.match(/^\.\/(\w+)\.png$/)[1]
-    return [emoteText.toLowerCase(), emoteRequire(k).default]
-  }),
-)
-const emoteNames = Array.from(emotes.keys())
-const emoteRegexp = new RegExp(
-  '(' + emoteNames.map((k) => `:${k}:`).join('|') + ')',
-  'i',
-)
+function loadEmotes() {
+  const emoteRequire = require.context('woke-content/emotes/', false, /\.png$/)
+  const emotes = new Map<string, string>(
+    emoteRequire.keys().map((k) => {
+      const emoteText = k.match(/^\.\/(\w+)\.png$/)[1]
+      return [':' + emoteText.toLowerCase() + ':', emoteRequire(k).default]
+    }),
+  )
+
+  const emoteAliases: {
+    [key: string]: string
+  } = require('woke-content/emotes/aliases.json')
+  for (const [alias, emote] of Object.entries(emoteAliases)) {
+    if (!emotes.has(emote)) {
+      console.warn('Unknown emote alias', alias, emote)
+      continue
+    }
+    emotes.set(alias, emotes.get(emote))
+  }
+
+  const emoteNames = Array.from(emotes.keys()).map(escapeStringRegexp)
+  const emoteRegexp = new RegExp('(' + emoteNames.join('|') + ')', 'i')
+
+  return { emotes, emoteRegexp }
+}
+
+const { emotes, emoteRegexp } = loadEmotes()
 
 type MessageProps = React.ComponentProps<typeof Text> & {
   body: string
@@ -28,7 +44,7 @@ export function Emote({ emote }: { emote: string }) {
       display="inline-block"
       src={emotes.get(emote)}
       boxSize={emoteSize + 'px'}
-      title={`:${emote}:`}
+      title={emote}
     />
   )
 }
@@ -36,7 +52,7 @@ export function Emote({ emote }: { emote: string }) {
 export default function Message({ body, sender, ...props }: MessageProps) {
   const parts = []
   for (const part of body.split(emoteRegexp)) {
-    const emote = part.substring(1, part.length - 1).toLowerCase()
+    const emote = part.toLowerCase()
     if (emotes.has(emote)) {
       parts.push(<Emote key={parts.length} emote={emote} />)
     } else {
