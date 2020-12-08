2b4e26e5f61dbf2a652a0de6c45090ba2075f033
topeye <70822730+topeye@users.noreply.github.com>
2020-12-08 02:02:28 -0500

    Remove whitespace

--- src/components/OfflinePlaceholder.tsx
+++ src/components/OfflinePlaceholder.tsx
@@ -1,5 +1,13 @@
 import * as React from 'react'
-import { Box, Center, Text, chakra, VStack, Button, ButtonGroup } from '@chakra-ui/react'
+import {
+  Box,
+  Center,
+  Text,
+  chakra,
+  VStack,
+  Button,
+  ButtonGroup,
+} from '@chakra-ui/react'
 
 import BackgroundVideo from 'woke-content/images/bg.mp4'
 
@@ -9,18 +17,22 @@ export default function OfflinePlacholder() {
   return (
     <Box position="relative" maxHeight="full">
       <Center position="absolute" w="full" h="full" zIndex={100}>
-       <VStack>
-           <Text
-          fontFamily="Oswald"
-          fontSize="3xl"
-          color="flame.600"
-          textShadow="0 0 5px black"
-          whiteSpace="nowrap"
-        >
-          Searching for livestreams...
-        </Text>
-           <a href="/submit"><Button variant="outline" colorScheme="orange" size="sm">Submit</Button></a>
-       </VStack>
+        <VStack>
+          <Text
+            fontFamily="Oswald"
+            fontSize="3xl"
+            color="flame.600"
+            textShadow="0 0 5px black"
+            whiteSpace="nowrap"
+          >
+            Searching for livestreams...
+          </Text>
+          <a href="/submit">
+            <Button variant="outline" colorScheme="orange" size="sm">
+              Submit
+            </Button>
+          </a>
+        </VStack>
       </Center>
       <VideoEl src={BackgroundVideo} opacity=".075" autoPlay muted loop />
     </Box>
