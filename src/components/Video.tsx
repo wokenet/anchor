9d3c9b357409178e16989b6a87d62f94aca329a5
Max Goodhart <c@chromakode.com>
2020-12-10 20:58:18 -0800

    Lengthen video control hide timeout

--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -118,7 +118,7 @@ const Video = forwardRef(
       }
       const timeout = setTimeout(() => {
         setIsInteractingControls(false)
-      }, 2 * 1000)
+      }, 5 * 1000)
       return () => clearTimeout(timeout)
     }, [isInteractingControls])
 
