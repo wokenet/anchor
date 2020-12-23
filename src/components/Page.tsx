d9d94115dac6bb13b46a29eb0e4b0ef224b1a39a
Max Goodhart <c@chromakode.com>
2020-12-23 01:06:31 -0800

    Prettier

--- src/components/Page.tsx
+++ src/components/Page.tsx
@@ -26,18 +26,39 @@ export default function Page({
           {title ? ' – ' : ''}WOKE.NET LIVE STREAM - GET ENGAGED - STAY WOKE!
         </title>
         <link rel="icon" href="/static/favicon.png"></link>
-        <meta name="description" content="Watch live social movements, collective action &amp; civic engagement. We access, amplify and archive historic livestreams of the disenfranchised." />
+        <meta
+          name="description"
+          content="Watch live social movements, collective action &amp; civic engagement. We access, amplify and archive historic livestreams of the disenfranchised."
+        />
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
         <meta property="og:type" content="website" />
         <meta property="og:url" content="https://woke.net/" />
-        <meta property="og:title" content="WOKE.NET LIVESTREAM - GET ENGAGED - STAY WOKE!" />
-        <meta property="og:description" content="Watch live social movements, collective action &amp; civic engagement. We access, amplify and archive historic livestreams of the disenfranchised." />
-        <meta property="og:image" content="https://woke.net/static/woke-social.jpg" />
+        <meta
+          property="og:title"
+          content="WOKE.NET LIVESTREAM - GET ENGAGED - STAY WOKE!"
+        />
+        <meta
+          property="og:description"
+          content="Watch live social movements, collective action &amp; civic engagement. We access, amplify and archive historic livestreams of the disenfranchised."
+        />
+        <meta
+          property="og:image"
+          content="https://woke.net/static/woke-social.jpg"
+        />
         <meta property="twitter:card" content="summary_large_image" />
         <meta property="twitter:url" content="https://woke.net/" />
-        <meta property="twitter:title" content="WOKE.NET LIVESTREAM - GET ENGAGED - STAY WOKE!" />
-        <meta property="twitter:description" content="Watch live social movements, collective action &amp; civic engagement. We access, amplify and archive historic livestreams of the disenfranchised." />
-        <meta property="twitter:image" content="https://woke.net/static/woke-social.jpg" />
+        <meta
+          property="twitter:title"
+          content="WOKE.NET LIVESTREAM - GET ENGAGED - STAY WOKE!"
+        />
+        <meta
+          property="twitter:description"
+          content="Watch live social movements, collective action &amp; civic engagement. We access, amplify and archive historic livestreams of the disenfranchised."
+        />
+        <meta
+          property="twitter:image"
+          content="https://woke.net/static/woke-social.jpg"
+        />
       </Head>
       <GATag />
       <Flex
