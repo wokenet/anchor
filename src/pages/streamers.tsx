d318e21651e369b2b76898138c5edc469040b4bb
Max Goodhart <c@chromakode.com>
2021-01-20 17:58:12 -0800

    Add streamer info popup

--- src/pages/streamers.tsx
+++ src/pages/streamers.tsx
@@ -1,7 +1,43 @@
 import * as React from 'react'
+import { useState } from 'react'
 import { GetStaticProps, InferGetStaticPropsType } from 'next'
-import { AspectRatio, Flex, SimpleGrid, Text } from '@chakra-ui/react'
+import {
+  AspectRatio,
+  Divider,
+  Flex,
+  Heading,
+  HStack,
+  Icon,
+  Image,
+  Link as ChakraLink,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalOverlay,
+  SimpleGrid,
+  Tag,
+  Text,
+  VStack,
+  Wrap,
+  WrapItem,
+} from '@chakra-ui/react'
 import { useInView } from 'react-intersection-observer'
+import {
+  FaYoutube,
+  FaFacebook,
+  FaTwitter,
+  FaInstagram,
+  FaPeriscope,
+  FaTwitch,
+  FaLink,
+  FaPaypal,
+  FaPatreon,
+  FaGlasses,
+} from 'react-icons/fa'
+import { IoLogoVenmo } from 'react-icons/io5'
+import { SiCashapp } from 'react-icons/si'
 
 import Page from 'src/components/Page'
 
@@ -9,13 +45,31 @@ type Streamer = {
   id: string
   photo: string
   name: string
+  info: string
+  home?: string
+  creators?: Array<string>
+  teams?: Array<string>
+  cities: Array<string>
+  website?: string
+  facebook?: string
+  youtube?: string
+  twitch?: string
+  twitter?: string
+  instagram?: string
+  periscope?: string
+  cashapp?: string
+  paypal?: string
+  venmo?: string
+  patreon?: string
+  streamlabs?: string
 }
 
 type StreamerTileProps = {
   streamer: Streamer
+  onClick: () => void
 }
 
-function StreamerTile({ streamer }: StreamerTileProps) {
+function StreamerTile({ streamer, onClick }: StreamerTileProps) {
   const [tileRef, inView] = useInView({
     triggerOnce: true,
     rootMargin: '200px 0px',
@@ -34,6 +88,7 @@ function StreamerTile({ streamer }: StreamerTileProps) {
         cursor="pointer"
         transitionDuration="slow"
         _hover={{ bgColor: 'orangeYellow.700' }}
+        onClick={onClick}
       >
         <Text
           overflow="hidden"
@@ -58,14 +113,225 @@ function StreamerTile({ streamer }: StreamerTileProps) {
   )
 }
 
+type StreamerModalProps = {
+  streamer: Streamer
+  isOpen: boolean
+  onClose: () => void
+}
+
+function StreamerModal({ streamer, isOpen, onClose }: StreamerModalProps) {
+  const hasSupportLinks =
+    streamer?.cashapp ||
+    streamer?.paypal ||
+    streamer?.venmo ||
+    streamer?.patreon ||
+    streamer?.streamlabs
+  return (
+    <Modal
+      size="3xl"
+      isOpen={isOpen}
+      onClose={onClose}
+      scrollBehavior="inside"
+      isCentered
+    >
+      <ModalOverlay />
+      {streamer && (
+        <ModalContent
+          overflow="hidden"
+          boxShadow="0 0 24px rgba(255, 255, 255, .2)"
+        >
+          <ModalCloseButton />
+          <ModalBody p={0}>
+            <Image src={streamer.photo} w="full" />
+            <Flex flexDir="column" mx={6} my={4}>
+              <Wrap spacing={4} mb={2}>
+                <WrapItem my={0}>
+                  <ChakraLink href={streamer.website} isExternal>
+                    <Heading display="inline" color="orangeYellow.300">
+                      {streamer.name}
+                    </Heading>
+                  </ChakraLink>
+                </WrapItem>
+                <WrapItem alignItems="center" my={0}>
+                  <HStack fontSize="xl">
+                    {streamer.facebook && (
+                      <ChakraLink href={streamer.facebook} isExternal>
+                        <Icon as={FaFacebook} />
+                      </ChakraLink>
+                    )}
+                    {streamer.youtube && (
+                      <ChakraLink href={streamer.youtube} isExternal>
+                        <Icon as={FaYoutube} />
+                      </ChakraLink>
+                    )}
+                    {streamer.twitch && (
+                      <ChakraLink href={streamer.twitch} isExternal>
+                        <Icon as={FaTwitch} />
+                      </ChakraLink>
+                    )}
+                    {streamer.twitter && (
+                      <ChakraLink href={streamer.twitter} isExternal>
+                        <Icon as={FaTwitter} />
+                      </ChakraLink>
+                    )}
+                    {streamer.instagram && (
+                      <ChakraLink href={streamer.instagram} isExternal>
+                        <Icon as={FaInstagram} />
+                      </ChakraLink>
+                    )}
+                    {streamer.periscope && (
+                      <ChakraLink href={streamer.periscope} isExternal>
+                        <Icon as={FaPeriscope} />
+                      </ChakraLink>
+                    )}
+                  </HStack>
+                </WrapItem>
+              </Wrap>
+              <VStack spacing={2} mt={2} alignItems="flex-start">
+                <Wrap spacing={2} color="gray.500">
+                  {streamer.creators?.length && (
+                    <WrapItem>
+                      <Text fontWeight="bold">
+                        {streamer.creators.join(', ')}
+                      </Text>
+                    </WrapItem>
+                  )}
+                  {streamer.website && (
+                    <WrapItem>
+                      <ChakraLink
+                        color="orangeYellow.300"
+                        href={streamer.website}
+                        isExternal
+                      >
+                        <Icon as={FaLink} mr={1} />
+                        website
+                      </ChakraLink>
+                    </WrapItem>
+                  )}
+                  <WrapItem>
+                    {streamer.home && <Text>{streamer.home}</Text>}
+                  </WrapItem>
+                </Wrap>
+                {streamer.info && <Text my={1}>{streamer.info}</Text>}
+                {streamer.teams && (
+                  <Wrap spacing={1}>
+                    {streamer.teams.map((t) => (
+                      <WrapItem key={t}>
+                        <Tag
+                          bg="orangeYellow.400"
+                          color="orangeYellow.800"
+                          px={2}
+                          borderRadius="4px"
+                          opacity=".75"
+                        >
+                          {t}
+                        </Tag>
+                      </WrapItem>
+                    ))}
+                  </Wrap>
+                )}
+                {streamer.teams && streamer.teams.length > 1 && (
+                  <Wrap spacing={1}>
+                    {streamer.cities.map((c) => (
+                      <WrapItem key={c}>
+                        <Tag
+                          bg="gray.600"
+                          px={2}
+                          borderRadius="4px"
+                          opacity=".75"
+                        >
+                          {c}
+                        </Tag>
+                      </WrapItem>
+                    ))}
+                  </Wrap>
+                )}
+              </VStack>
+              {hasSupportLinks && (
+                <>
+                  <Divider pt={4} />
+                  <ModalFooter
+                    color="flame.300"
+                    alignItems="center"
+                    justifyContent="flex-start"
+                    px={0}
+                    pb={0}
+                  >
+                    <Heading as="h3" size="md" display="inline">
+                      Support {streamer.name}:
+                    </Heading>
+                    <HStack ml={4} spacing={4} fontSize="2rem">
+                      {streamer.cashapp && (
+                        <ChakraLink
+                          display="flex"
+                          href={streamer.cashapp}
+                          isExternal
+                        >
+                          <Icon as={SiCashapp} />
+                        </ChakraLink>
+                      )}
+                      {streamer.paypal && (
+                        <ChakraLink
+                          display="flex"
+                          href={streamer.paypal}
+                          isExternal
+                        >
+                          <Icon as={FaPaypal} />
+                        </ChakraLink>
+                      )}
+                      {streamer.venmo && (
+                        <ChakraLink
+                          display="flex"
+                          href={streamer.venmo}
+                          isExternal
+                        >
+                          <Icon as={IoLogoVenmo} />
+                        </ChakraLink>
+                      )}
+                      {streamer.patreon && (
+                        <ChakraLink
+                          display="flex"
+                          href={streamer.patreon}
+                          isExternal
+                        >
+                          <Icon as={FaPatreon} />
+                        </ChakraLink>
+                      )}
+                      {streamer.streamlabs && (
+                        <ChakraLink
+                          display="flex"
+                          href={streamer.streamlabs}
+                          isExternal
+                        >
+                          <Icon as={FaGlasses} />
+                        </ChakraLink>
+                      )}
+                    </HStack>
+                  </ModalFooter>
+                </>
+              )}
+            </Flex>
+          </ModalBody>
+        </ModalContent>
+      )}
+    </Modal>
+  )
+}
+
 export default function StreamersPage({
   streamers,
 }: InferGetStaticPropsType<typeof getStaticProps>) {
+  const [selectedStreamer, setSelectedStreamer] = useState<Streamer>()
   const streamersWithPhotos: Array<Streamer> = streamers.filter(
     (s) => !!s.photo,
   )
   return (
     <Page title="streamers">
+      <StreamerModal
+        streamer={selectedStreamer}
+        isOpen={!!selectedStreamer}
+        onClose={() => setSelectedStreamer(undefined)}
+      />
       <SimpleGrid
         templateColumns="repeat(auto-fill, minmax(18rem, 1fr))"
         spacing={1}
@@ -73,7 +339,11 @@ export default function StreamersPage({
         py={1}
       >
         {streamersWithPhotos.map((s) => (
-          <StreamerTile key={s.id} streamer={s} />
+          <StreamerTile
+            key={s.id}
+            streamer={s}
+            onClick={() => setSelectedStreamer(s)}
+          />
         ))}
       </SimpleGrid>
     </Page>
